3b4b38decaeb8a326a2d8040dad81a16ce8b60ce
btabatow <44070309+btabatow@users.noreply.github.com>
2018-10-11 14:59:08 -0400

    Create Notes.py

---
+++ Notes.py
@@ -0,0 +1,8 @@
+
+#Tested commands
+libardrone.ARDrone()
+# if no connection socket.err is thrown
+# if connects no response
+# No commands have a response
+# above call returns drone object, intializer for ARDrone object
+
